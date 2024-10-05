78ae36bfca4f9a4273f9e1f55dd964bee0217bb3
Adrian Pineda <apinedabawork@gmail.com>
2024-10-04 22:49:48 -0600

    revert axel api

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,14 @@ use App\Http\Controllers\AddressController;
 use App\Http\Controllers\Auth\UserAuthController;
 use App\Http\Controllers\Auth\PatientAuthController;
 use App\Http\Middleware\HandleInvalidToken;
-
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\PatientUserController;
 
 
 
 //Rutas para profesionales
 Route::post('user/login', [UserAuthController::class, 'login']);
+Route::post('user/register', [RegisterController::class, 'registerUser']);
 Route::middleware(['auth:sanctum', 'handle_invalid_token', 'user'])->group(function () {
     Route::get('user/info', function (Request $request) {
         return $request->user();
@@ -27,6 +29,8 @@ Route::middleware(['auth:sanctum', 'handle_invalid_token', 'user'])->group(funct
     Route::resource('user/education', EducationUserController::class);
     Route::resource('user/address', AddressController::class);
     Route::resource('user/profile', ProfileController::class);
+    Route::resource('user/patient', PatientController::class);
+    Route::resource('user/catalog/patients', PatientUserController::class);
 });
 
 

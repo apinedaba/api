4e058bf67831a327f80ad7d0e061b619594635f7
Adrian Pineda <adrian.pineda@infotec.mx>
2025-06-26 03:01:28 -0600

    carritos, y match de citas

---
+++ app/Models/AppointmentCart.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppointmentCart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'patient_id', 'user_id',
+        'fecha', 'hora',
+        'tipoSesion', 'duracion', 'precio', 'estado'
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

790769b093e73f80a6253278296d7185d0fad584
Adrian Pineda <adrian.pineda@infotec.mx>
2025-07-04 19:33:12 -0600

    crear horarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'contacto',
         'educacion',
         'configurations',
+        'horarios',
         'plan',
         'image'
     ];
@@ -55,6 +56,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'educacion' => 'array',
         'personales' => 'array',
         'configurations' => 'array',
+        'horarios' => 'array',
         'image' => 'string',
         'isProfileComplete' => 'boolean',
     ];

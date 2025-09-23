f4281d5079142f806cf7fe4f5b1974e867d70841
Adrian Pineda <totalcontrolmusic@gmail.com>
2025-09-22 18:12:11 -0600

    Contratos final

--- routes/api/contratos.php
+++ routes/api/contratos.php
@@ -2,9 +2,9 @@
 // routes/api.php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{ContractTemplateController, ContractInstanceController};
+use App\Http\Controllers\{ContractTemplateController, ContractInstanceController, ContractAssignmentController};
 
-Route::prefix('user/contracts')->middleware('auth:sanctum')->group(function() {
+Route::prefix('user/contracts')->middleware(['auth:sanctum', 'handle_invalid_token', 'user'])->group(function () {
   // Plantillas
   Route::get('/templates', [ContractTemplateController::class, 'index']);
   Route::post('/templates', [ContractTemplateController::class, 'store']);
@@ -18,5 +18,25 @@ Route::prefix('user/contracts')->middleware('auth:sanctum')->group(function() {
   Route::get('/instances/{instance}', [ContractInstanceController::class, 'show']);
   Route::post('/instances/{instance}/signature', [ContractInstanceController::class, 'signature']); // Canvas → PNG
   Route::post('/instances/{instance}/finalize', [ContractInstanceController::class, 'finalize']);   // DOMPDF + Cloudinary
-  Route::post('/instances/{instance}/upload', [ContractInstanceController::class, 'upload']);       // Camino 2
+  Route::post('/instances/{instance}/upload', [ContractInstanceController::class, 'upload']);
+
+  // Camino 2
+  Route::get('/assignments', [ContractAssignmentController::class, 'index']);
+  Route::get('/assignments/{id}', [ContractAssignmentController::class, 'show']);
+  Route::post('/assignments', [ContractAssignmentController::class, 'store']);
+  Route::post('/assignments/{id}/signature', [ContractAssignmentController::class, 'signature']);
+  Route::post('/assignments/{id}/finalize', [ContractAssignmentController::class, 'finalize']);
+  Route::get('/assignments/{id}/pdf', [ContractAssignmentController::class, 'downloadPdf']);
+
+});
+
+
+Route::prefix('patient/contracts')->middleware(['auth:sanctum', 'handle_invalid_token', 'patient'])->group(function () {
+  Route::get('/assignments', [ContractAssignmentController::class, 'index']);
+  Route::get('/assignments/{id}', [ContractAssignmentController::class, 'show']);
+  Route::post('/assignments', [ContractAssignmentController::class, 'store']);
+  Route::post('/assignments/{id}/signature', [ContractAssignmentController::class, 'signature']);
+  Route::post('/assignments/{id}/finalize', [ContractAssignmentController::class, 'finalize']);
+  Route::get('/assignments/{id}/pdf', [ContractAssignmentController::class, 'downloadPdf']);
+
 });

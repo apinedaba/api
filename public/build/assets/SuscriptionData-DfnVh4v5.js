2ccb373667200e5324ab7eaf2febfac27281e892
Adrian Pineda <apinedabawork@gmail.com>
2026-02-05 15:46:09 -0600

    build

--- public/build/assets/SuscriptionData-DfnVh4v5.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-cFKHF5XC.js";function r(){return t.jsx("div",{children:t.jsx("h1",{children:"Suscription Data"})})}export{r as default};
---
+++ public/build/assets/SuscriptionData-FbIGhUKo.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-B1mEVFSb.js";function r(){return t.jsx("div",{children:t.jsx("h1",{children:"Suscription Data"})})}export{r as default};

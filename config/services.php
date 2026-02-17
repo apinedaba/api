4692e367784f7dc240e3d6515a54412c29821204
Jesus Hernandez <jesus@reevolution.com.mx>
2026-02-16 18:12:51 -0600

    Agregar claves de Stripe en la configuración de servicios

--- config/services.php
+++ config/services.php
@@ -26,6 +26,8 @@ return [
     ],
 
     'stripe' => [
+        'secret_key' => env('STRIPE_SECRET_KEY'),
+        'public_key' => env('STRIPE_PUBLIC_KEY'),
         'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],
 

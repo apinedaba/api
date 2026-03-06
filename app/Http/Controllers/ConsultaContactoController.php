f5c3fba5b626bbece9c564a14f621635e07a32d4
Adrian Pineda <apinedabawork@gmail.com>
2026-03-05 21:58:13 -0600

    fix axeel

--- app/Http/Controllers/ConsultaContactoController.php
+++ app/Http/Controllers/ConsultaContactoController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\ConsultaContacto;
+use App\Notifications\NotificacionPsicologo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Notifications\NuevoContacto;
-use App\Notifications\NuevoPosiblePaciente;
-use App\Notifications\ConfirmacionPaciente;
+use App\Notifications\NuevoPosibleContacto;
 
 class ConsultaContactoController extends Controller
 {
@@ -33,14 +33,14 @@ class ConsultaContactoController extends Controller
 
         $consulta = ConsultaContacto::create($request->all());
         try {
-            $consulta->notify(new ConfirmacionPaciente());
-            
+            $consulta->notify(new NuevoContacto($consulta));
             $psicologo = \App\Models\User::find($request->user_id);
-            if ($psicologo) {               
-                $psicologo->notify(new NotificacionPsicologo($consulta));
+            \Log::info("PSICOLGO: " . $psicologo);
+            if ($psicologo) {
+                $psicologo->notify(new NotificacionPsicologo($consulta, $psicologo));
             }
         } catch (\Throwable $th) {
-            \Log::error("ERROR REAL: " . $th->getMessage());
+            \Log::error("Error enviando notificación de contacto: " . $th->getMessage());
         }
 
         return response()->json([
@@ -51,10 +51,10 @@ class ConsultaContactoController extends Controller
     }
     public function getData()
     {
-        $userId = auth()->id(); 
+        $userId = auth()->id();
         $consultas = \App\Models\ConsultaContacto::where('user_id', $userId)
-                        ->latest()
-                        ->get();
+            ->latest()
+            ->get();
 
         return response()->json([
             'status' => 'success',
